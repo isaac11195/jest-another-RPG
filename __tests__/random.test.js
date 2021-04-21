8d24ddbdf592297ce9157844644b0d6ed99c2631
Mona Lisa <isaac11195@gmail.com>
2021-04-20 22:49:20 -0400

    add new commands

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -1,4 +1,5 @@
-const checkIfEqual = require('../lib/random.js');
+const randomNumber = require('../lib/random.js');
+
 test('gets random number between 1 and 10', () => {
   expect(randomNumber()).toBeGreaterThanOrEqual(1);
   expect(randomNumber()).toBeLessThanOrEqual(10);
